5c408d8ffd206179e60c332261d017fc806e8b33
Frederico Ramos <frediramos@gmail.com>
2022-12-09 00:26:34 +0000

    Remoxe extra examples

--- Examples/strcpy/concrete_strcpy.c
+++
@@ -1,9 +0,0 @@
-//Concrete implementation to be compared with
-char* concrete_strcpy(char* dest, char* source){
-	char* save = dest;
-	while (*source){
-	   *dest++ = *source++;
-	}
-	*dest = '\0';
-	return (char *) save;
-}
--- Examples/tolower/concrete_tolower.c
+++
@@ -1,6 +0,0 @@
-int concrete_tolower(int c){
-	if (c >= 65 && c <= 90){
-		return c + 32;
-	}
-	return c;
-}
